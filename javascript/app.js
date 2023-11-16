6f901308b274affc4267b3144829dede5990a98c
Tyler Hoppe <tylerhoppe@Tylers-MacBook-Pro.local>
2023-11-16 12:39:59 -0800

    Comments.

--- javascript/app.js
+++ javascript/app.js
@@ -11,14 +11,29 @@ let tree = new Tree();
 app.use(express.json());
 
 app.get('/', (req, res) => res.send("Couldn't see the forest for the trees!"));
+
+// Get the deep tree representation
 app.get('/api/tree', (req, res) => res.send(tree.deepTree));
+
+// Add a node to the tree. Parent id is optional.
+// If not provided, adds the node to the root of the tree. 
 app.post('/api/tree', (req, res) => res.send(addNode(tree, req.body)));
+
+// Reset the whole tree
 app.delete('/api/tree', (req, res) => {
   tree = new Tree();
   res.send(tree.deepTree);
 });
+
+// Get a tree branch at a given node.
 app.get('/api/tree/node/:id', (req, res) => res.send(getNode(tree, req.params.id)));
+
+// Update a node. If changing parent, an error will be thrown if it would create a cycle.
 app.put('/api/tree/node/:id', (req, res) => res.send(updateNode(tree, req.params.id, req.body)));
+
+// Delete a node.
+// Query parm keepChildren can be used to optionally preserve children by moving them to the parent,
+// otherwise, all children are deleted.
 app.delete('/api/tree/node/:id', (req, res) => res.send(deleteNode(tree, req.params.id, req.query.keepChildren)));
 
 // A custom middleware that will look for our error type for handling error responses.
